c690eed61d062be7ea0062c0a9cf5557e89cc64f
paul serafim <paul.serafim.3021@gmail.com>
2022-11-24 15:15:14 +1100

    message components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com', 'imgur.com', 'platform-lookaside.fbsbx.com', 'ibb.co']
+  },
+  experimental: {
+    appDir: true,
+  }
 }
